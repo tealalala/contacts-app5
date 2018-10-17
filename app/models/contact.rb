c6d53a932e5506cd7a3f918136f7037151ffc8e6
Tea <tdinhla@protonmail.com>
2018-10-17 16:53:47 -0700

    add interpolation in model/contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,12 +4,6 @@ class Contact < ApplicationRecord
   end
 
   def full_name
-    first_name + " " + last_name
+    "#{first_name} #{last_name}"
   end
-
-  # def japan_phone_number_prefix
-  #   if phonenumber.join.length == 8
-  #     phonenumber.
-  #   end
-  # end
 end
