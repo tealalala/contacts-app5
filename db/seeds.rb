18098d84106b18d6d37c06f9392c04dccd6e95c3
Tea <tdinhla@protonmail.com>
2018-10-18 17:03:18 -0700

    populate middle name and bio with seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do
-  contact = Contact.new(first_name: Faker::Name.first_name, middle_name: Faker::Name.middle_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number)
+# 5.times do
+#   contact = Contact.new(first_name: Faker::Name.first_name, middle_name: Faker::Name.middle_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number)
+#   contact.save
+# end
+
+contacts = Contact.all
+
+contacts.each do |contact|
+  contact.middle_name = Faker::Name.first_name
+  contact.bio = Faker::Hipster.paragraph
   contact.save
 end
