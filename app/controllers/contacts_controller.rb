13c759020a955a2defc7d699d2e20b1530be9e81
Tea <tdinhla@protonmail.com>
2018-11-05 17:39:43 -0800

    add index action and view for unnamespaced contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,48 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render 'index.html.erb'
+  end
+
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    render 'show.json.jbuilder'
+  end
+
+  def create
+    @contact = Contact.new(
+      user_id: current_user.id,
+      first_name: params[:input_first_name],
+      middle_name: params[:input_middle_name],
+      last_name: params[:input_last_name],
+      email: params[:input_email],
+      phone_number: params[:input_phone_number],
+      bio: params[:input_bio]
+    )
+    @contact.save
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    @contact.first_name = params[:input_first_name]
+    @contact.middle_name = params[:input_middle_name]
+    @contact.last_name = params[:input_last_name]
+    @contact.email = params[:input_email]
+    @contact.phone_number = params[:input_phone_number]
+    @contact.bio = params[:input_bio]
+    @contact.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    @contact.destroy
+    render 'destroy.json.jbuilder'
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
 
+  ## unnamespaced contacts
+  get '/contacts' => 'contacts#index'
+  # get '/contacts/new' => 'contacts#new'
+  # post '/contacts' => 'contacts#create'
+  # get '/contacts/:id' => 'contacts#show'
+  # get '/contacts/:id/edit' => 'contacts#edit'
+  # patch '/contacts/:id' => 'contacts#update'
+  # delete '/contacts/:id' => 'contacts#destroy'
+
+
   namespace :api do
     ## contacts
     get '/contacts' => 'contacts#index'
