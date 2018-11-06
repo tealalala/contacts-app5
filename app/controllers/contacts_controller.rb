a7bbb5773cfdc27999c52868d99257067889fb29
Tea <tdinhla@protonmail.com>
2018-11-05 18:20:41 -0800

    add new, create, edit, update, and destroy actions for unnamespaced contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,44 +4,50 @@ class ContactsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    @contacts = Contact.all
+    render 'new.html.erb'
+  end
+
   def show
     @contact = Contact.find_by(id: params[:id])
-    render 'show.index.erb'
+    render 'show.html.erb'
   end
 
   def create
     @contact = Contact.new(
-      user_id: current_user.id,
-      first_name: params[:input_first_name],
-      middle_name: params[:input_middle_name],
-      last_name: params[:input_last_name],
-      email: params[:input_email],
-      phone_number: params[:input_phone_number],
-      bio: params[:input_bio]
+      # user_id: current_user.id,
+      first_name: params[:first_name],
+      middle_name: params[:middle_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      bio: params[:bio]
     )
     @contact.save
-    render 'show.json.jbuilder'
+    render 'show.html.erb'
+  end
+
+  def edit
+    @contact = Contact.find_by(id: params[:id])
+    render 'edit.html.erb'
   end
 
   def update
-    contact_id = params[:id]
-    @contact = Contact.find_by(id: contact_id)
-    @contact.first_name = params[:input_first_name]
-    @contact.middle_name = params[:input_middle_name]
-    @contact.last_name = params[:input_last_name]
-    @contact.email = params[:input_email]
-    @contact.phone_number = params[:input_phone_number]
-    @contact.bio = params[:input_bio]
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name]
+    @contact.middle_name = params[:middle_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.phone_number = params[:phone_number]
+    @contact.bio = params[:bio]
     @contact.save
-    render 'show.json.jbuilder'
+    redirect_to '/contacts'
   end
 
   def destroy
-    contact_id = params[:id]
-    @contact = Contact.find_by(id: contact_id)
+    @contact = Contact.find_by(id: params[:id])
     @contact.destroy
-    render 'destroy.json.jbuilder'
+    redirect_to '/contacts'
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
 
   ## unnamespaced contacts
   get '/contacts' => 'contacts#index'
-  # get '/contacts/new' => 'contacts#new'
-  # post '/contacts' => 'contacts#create'
+  get '/contacts/new' => 'contacts#new'
+  post '/contacts' => 'contacts#create'
   get '/contacts/:id' => 'contacts#show'
-  # get '/contacts/:id/edit' => 'contacts#edit'
-  # patch '/contacts/:id' => 'contacts#update'
-  # delete '/contacts/:id' => 'contacts#destroy'
+  get '/contacts/:id/edit' => 'contacts#edit'
+  patch '/contacts/:id' => 'contacts#update'
+  delete '/contacts/:id' => 'contacts#destroy'
 
 
   namespace :api do
